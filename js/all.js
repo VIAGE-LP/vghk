616b8ad7ac603bc668784c241981cfe6de93efbf
Pickachu Chen <pickachu1006@gmail.com>
2021-04-08 18:00:36 +0800

    20210408_anniversary update

--- js/all.js
+++ js/all.js
@@ -26,4 +26,15 @@ $(document).ready(function () {
     } else {
         $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
     }
+
+    //週年慶活動辦法下拉選單
+   $(".anniversary_2,.SP_anniversary_2").hide();
+   $('.anniversary_1, .SP_anniversary_1').click(function (e) { 
+       e.preventDefault();
+       $(".anniversary_2,.SP_anniversary_2").toggle();;
+   });
+   $('.anniversary_2, .SP_anniversary_2').click(function (e) { 
+       e.preventDefault();
+       $(".anniversary_2, .SP_anniversary_2").hide();
+   });
 });
